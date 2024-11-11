0c3a3d85cee14b7659d09efefe2e540eb91f30cd
Binisha <binisha072@gmail.com>
2024-11-11 19:09:54 +0545

    predict by data and image

--- frontend main/src/App.tsx
+++ frontend main/src/App.tsx
@@ -1,18 +1,19 @@
 import { HomeScreen } from "./components/HomeScreen";
 import Login from "./components/Login";
+import PredictByData from "./components/PredictByData";
+import PredictByImage from "./components/PredictByImage";
 import Signup from "./components/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Signup" element={<Signup />} />
-      <Route path="/HomeScreen" element={<HomeScreen />} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PredictByImage />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/HomeScreen" element={<HomeScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend main/src/components/PredictByImage.tsx
@@ -0,0 +1,77 @@
+import React, { useRef } from "react";
+import Navigation from "./Navigation";
+
+const PredictByImage = () => {
+  const fileInputRef = useRef(null); // create a ref for the file input
+
+  const handleFileChange = () => {
+    const fileName = fileInputRef.current?.files[0]?.name;
+    if (fileName) {
+      document.getElementById(
+        "file-name"
+      ).textContent = `Selected File: ${fileName}`;
+    }
+  };
+
+  return (
+    <div>
+      <Navigation />
+      <h1 className="text-center mt-20 font-bold text-3xl mb-10">
+        Predict Kidney Stone By Image
+      </h1>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-96">
+          <label
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+          >
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg
+                className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 16"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                />
+              </svg>
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG or GIF (MAX. 800x400px)
+              </p>
+            </div>
+            <input
+              id="dropzone-file"
+              type="file"
+              accept="image/*"
+              className="hidden"
+              ref={fileInputRef} // attach ref
+              onChange={handleFileChange} // handle file change
+            />
+          </label>
+        </div>
+        {/* Display the file name */}
+        <div className="text-center mt-4">
+          <p id="file-name" className="text-xl font-semibold"></p>
+        </div>
+        <button
+          type="submit"
+          className="mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PredictByImage;
